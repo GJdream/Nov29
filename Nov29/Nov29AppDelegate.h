f546663b06339b31d65903b7ed045b696d53fcf9
Lisa <ljenkin2@hotmail.com>
2012-11-27 19:56:46 -0500

    Initial Commit

---
+++ Nov29/Nov29AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  Nov29AppDelegate.h
+//  Nov29
+//
+//  Created by Lisa Jenkins on 11/27/12.
+//  Copyright (c) 2012 Lisa Jenkins. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Nov29AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Nov29/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Nov29
+//
+//  Created by Lisa Jenkins on 11/27/12.
+//  Copyright (c) 2012 Lisa Jenkins. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "Nov29AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([Nov29AppDelegate class]));
+    }
+}
